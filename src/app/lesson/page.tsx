ceafe83a54bf79f2ad8886e6d2c43c94dc9252a5
iliapovar15-pixel <ilia.povar15@gmail.com>
2026-01-03 17:48:53 +0300

    Update page.tsx
    "../../components/LessonContent";

--- src/app/lesson/page.tsx
+++ src/app/lesson/page.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from "react";
-import { LessonContent } from "@/components/LessonContent";
+import { LessonContent } from "../../components/LessonContent";
 
 function LessonLoading() {
   return (
